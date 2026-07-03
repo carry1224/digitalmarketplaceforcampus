e7409a8b06ca30f7e86f8817caa167d7e6ed5a8c
carry1224 <carrycathrine.gmail.com>
2026-07-03 07:17:51 +0300

    added html code for the log in page

---
+++ code.js
@@ -0,0 +1,7 @@
+const startBtn = document.getElementById("start-btn");
+
+if (start-btn) {
+    startBtn.addEventListener("click", function () {
+        window.location.href = "login.html";
+    });
+}
